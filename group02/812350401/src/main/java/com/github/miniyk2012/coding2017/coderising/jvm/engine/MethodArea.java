c307bb659bad42c51727b50aec0758b0f2a46149
thomas_young <yk_ecust_2007@163.com>
2017-06-07 07:14:04 +0800

    Method area done

--- group02/812350401/src/main/java/com/github/miniyk2012/coding2017/coderising/jvm/engine/MethodArea.java
+++ group02/812350401/src/main/java/com/github/miniyk2012/coding2017/coderising/jvm/engine/MethodArea.java
@@ -55,14 +55,27 @@ public class MethodArea {
 	
 	
 	public Method getMethod(String className, String methodName, String paramAndReturnType){
-		
-		return null;
+		ClassFile clz = findClassFile(className);
+		Method m =  clz.getMethod(methodName, paramAndReturnType);
+        if(m == null){
+
+            throw new RuntimeException("method can't be found : \n"
+                    + "class: " + className
+                    + "method: " + methodName
+                    + "paramAndReturnType: " + paramAndReturnType);
+        }
+        return m;
 	}
 	
 	
-	public Method getMethod(MethodRefInfo methodRef){		
-		
-		return null;
-			
+	public Method getMethod(MethodRefInfo methodRef){
+        ClassFile clz = findClassFile(methodRef.getClassName());
+		String methodName = methodRef.getMethodName();
+		String paramAndReturnType = methodRef.getParamAndReturnType();
+        Method m =  clz.getMethod(methodName, paramAndReturnType);
+        if (m == null) {
+            throw new RuntimeException("method can't be found : " + methodRef.toString());
+        }
+        return m;
 	}
 }
